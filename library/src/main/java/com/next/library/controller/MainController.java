43576ce87ef1ebf90f32706a65503fd2f40b6e8d
Elison Evaristo Souza <elison_souza15@hotmail.com>
2017-09-23 19:31:52 -0300

    alteração layout

--- library/src/main/java/com/next/library/controller/MainController.java
+++ library/src/main/java/com/next/library/controller/MainController.java
@@ -22,54 +22,53 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class MainController {
-    
+
     IProdutoRepository _repository;
-    
-    protected MainController(IProdutoRepository repository){
+
+    protected MainController(IProdutoRepository repository) {
         _repository = repository;
     }
-    
-    @RequestMapping(value="/", method=RequestMethod.GET)
-    public String index(HttpServletRequest request) throws IOException, URISyntaxException {        
-        
-        request.getSession().setAttribute("carrinho", new Carrinho());  
-        
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(HttpServletRequest request) throws IOException, URISyntaxException {
+
+        request.getSession().setAttribute("carrinho", new Carrinho());
+
         //IOUtils.toByteArray(ClassLoader.class.getResourceAsStream("como-a-mente-funciona.jpg"))
-        
-        Path path01 = Paths.get("C:/Users/roger.roliveira/Downloads", "como-a-mente-funciona.jpg");
-        Path path02 = Paths.get("C:/Users/roger.roliveira/Downloads", "do-que-e-feito-o-pensamento.jpg");
-        Path path03 = Paths.get("C:/Users/roger.roliveira/Downloads", "o-orfanato-da-srta-peregrine.jpg");
+        Path path01 = Paths.get("C:\\Users\\Adonis\\Documents\\NetBeansProjects\\nextlib-spring\\library\\src\\main\\resources\\images", "como-a-mente-funciona.jpg");
+        Path path02 = Paths.get("C:\\Users\\Adonis\\Documents\\NetBeansProjects\\nextlib-spring\\library\\src\\main\\resources\\images", "do-que-e-feito-o-pensamento.jpg");
+        Path path03 = Paths.get("C:\\Users\\Adonis\\Documents\\NetBeansProjects\\nextlib-spring\\library\\src\\main\\resources\\images", "o-orfanato-da-srta-peregrine.jpg");
 
         _repository.save(
                 new Produto(
-                    1, 
-                    "Como a mente funciona", 
-                    "Como as crianças aprendem sobre o mundo que as rodeia? "
-                    + "Pinker mostra como podemos estar bem próximos de uma das últimas fronteiras do conhecimento - "
-                    + "a mente humana.",
-                    47.9,
-                Files.readAllBytes(path01)));        
-        
+                        1,
+                        "Como a mente funciona",
+                        "Como as crianças aprendem sobre o mundo que as rodeia? "
+                        + "Pinker mostra como podemos estar bem próximos de uma das últimas fronteiras do conhecimento - "
+                        + "a mente humana.",
+                        47.9,
+                        Files.readAllBytes(path01)));
+
         _repository.save(
                 new Produto(
-                    2, 
-                    "Do que é feito o pensamento", 
-                    "Combinando alguns de seus livros anteriores como O instinto da linguagem (1994) e "
-                    + "Como a mente funciona (1998), Steven Pinker encontra na linguagem uma janela para uma "
-                    + "possível explicação da natureza humana.",
-                    52.9,
-                Files.readAllBytes(path02)));        
-        
+                        2,
+                        "Do que é feito o pensamento",
+                        "Combinando alguns de seus livros anteriores como O instinto da linguagem (1994) e "
+                        + "Como a mente funciona (1998), Steven Pinker encontra na linguagem uma janela para uma "
+                        + "possível explicação da natureza humana.",
+                        52.9,
+                        Files.readAllBytes(path02)));
+
         _repository.save(
                 new Produto(
-                    3, 
-                    "O Orfanato da Srta. Peregrine Para Crianças Peculiares", 
-                    "O orfanato da Srta. Peregrine para Crianças Peculiares é um romance que "
-                    + "mistura ficção e fotografia. A história começa com uma tragédia familiar que lança Jacob, "
-                            + "um rapaz de 16 anos, em uma jornada até uma ilha remota na costa do País de Gales.",
-                    13.9,
-                Files.readAllBytes(path03)));        
-        
+                        3,
+                        "O Orfanato da Srta. Peregrine",
+                        "O orfanato da Srta. Peregrine para Crianças Peculiares é um romance que "
+                        + "mistura ficção e fotografia. A história começa com uma tragédia familiar que lança Jacob, "
+                        + "um rapaz de 16 anos, em uma jornada até uma ilha remota na costa do País de Gales.",
+                        13.9,
+                        Files.readAllBytes(path03)));
+
         return "index";
     }
 }
