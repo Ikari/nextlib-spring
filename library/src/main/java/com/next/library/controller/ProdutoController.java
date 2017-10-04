beec62ddef351681af91ceaf7028e0279c106f3a
ROGER RODRIGUES DE OLIVEIRA <roger.roliveira@senacsp.edu.br>
2017-10-03 22:29:28 -0300

    Tentativa de teste

--- library/src/main/java/com/next/library/controller/ProdutoController.java
+++ library/src/main/java/com/next/library/controller/ProdutoController.java
@@ -1,18 +1,14 @@
 package com.next.library.controller;
 
-import com.next.library.model.Carrinho;
 import com.next.library.model.Produto;
 import com.next.library.repository.IProdutoRepository;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
