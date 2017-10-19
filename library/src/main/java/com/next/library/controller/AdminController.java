44b44c8e14911e71517553e0435f1a9f61ee90db
ROGER RODRIGUES DE OLIVEIRA <roger.roliveira@senacsp.edu.br>
2017-10-18 22:31:07 -0200

    Edição de produtos

--- library/src/main/java/com/next/library/controller/AdminController.java
+++ library/src/main/java/com/next/library/controller/AdminController.java
@@ -6,13 +6,16 @@
 package com.next.library.controller;
 
 import com.next.library.model.Produto;
+import com.next.library.repository.IProdutoExcertpRepository;
 import com.next.library.repository.IProdutoRepository;
 import java.io.IOException;
 import javax.validation.Valid;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,10 +32,21 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class AdminController {
     
     @Autowired private IProdutoRepository produtoRepository;
+    @Autowired private IProdutoExcertpRepository produtoRepositoryDto;
     
-    @RequestMapping("/produto")
+    @RequestMapping("/produto/lista")
+    public ModelAndView listaProdutos(){
+        return new ModelAndView("/admin/produto/lista").addObject("produtos", produtoRepositoryDto.findAll());
+    }
+    
+    @RequestMapping("/produto/cadastro")
     public ModelAndView cadastro(){        
         return new ModelAndView("/admin/produto/cadastro").addObject("produto", new Produto());        
+    }
+    
+    @RequestMapping("/produto/cadastro/{id}")
+    public ModelAndView editar(@PathVariable("id") ObjectId id){                
+        return new ModelAndView("/admin/produto/cadastro").addObject("produto", produtoRepository.findOne(id));        
     }
     
     @RequestMapping(value="/produto/cadastrar", method=RequestMethod.POST)
