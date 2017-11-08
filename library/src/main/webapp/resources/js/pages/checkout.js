d190ab7db1b7ce9fed1db76760c66b043c560fde
togger <roger.ikari@gmail.com>
2017-11-08 12:24:42 -0200

    Ajustes no fluxo de checkout e relatório

--- library/src/main/webapp/resources/js/pages/checkout.js
+++ library/src/main/webapp/resources/js/pages/checkout.js
@@ -13,6 +13,9 @@ $(function(){
         $("#tabs li").removeClass("active").addClass("inactive");
         a.parent().removeClass("inactive").addClass("active");
         $("#content").load("/checkout/" + a.attr("id")); 
+        
+        if (parseInt(a.attr("id")) === 3)
+            $("#save-and-continue").hide();
     };
     
     this.save = function(){
@@ -24,8 +27,7 @@ $(function(){
                 method: "POST",
                 url: "/checkout/etapa/" + activeTab.attr("id"),
                 data: $("#form-checkout").serialize(),
-                success: function(data){ 
-                    
+                success: function(data){                     
                     $("#" + (parseInt(activeTab.attr("id")) + 1).toString()).click();
                 },
                 error: function(e){ console.log(e); }
